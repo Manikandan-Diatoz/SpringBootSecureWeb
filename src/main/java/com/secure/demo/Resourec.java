00cd5afaab4b2a85d82c775c632db12dc33a7fe1
Manikandan-Diatoz <bmanikandan034@gmail.com>
2019-10-21 10:52:11 +0530

    Initial commit

---
+++ src/main/java/com/secure/demo/Resourec.java
@@ -0,0 +1,25 @@
+package com.secure.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Resourec {
+
+	@GetMapping("/")
+	public String home()
+	{
+		return "welcome ";
+	}
+	@GetMapping("/user")
+	public String user()
+	{
+		return "Welcome user";
+	}
+	
+	@GetMapping("/admin")
+	public String admin()
+	{
+		return "Welcome Admin";
+	}
+}
